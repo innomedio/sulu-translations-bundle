4b48aa28bcbe95f343bd17a8f90bc9607f3d9922
Ewald Vanderveken <ewald.vanderveken@innomedio.be>
2026-01-15 13:06:04 +0100

    Fix configuration

--- src/Infrastructure/Symfony/Translation/Export/CliExportAction.php
+++ src/Infrastructure/Symfony/Translation/Export/CliExportAction.php
@@ -11,12 +11,12 @@ use Phpro\SuluTranslationsBundle\Infrastructure\Symfony\Translation\Provider\Dat
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 final readonly class CliExportAction implements ExportAction
 {
     public function __construct(
-        private KernelInterface $kernel,
+        private string $environment,
+        private bool $debug,
         private string $exportFormat,
     ) {
     }
@@ -51,8 +51,8 @@ final readonly class CliExportAction implements ExportAction
     private function executeCommand(ArrayInput $arrayInput, string $context = 'website'): void
     {
         $kernel = new Kernel(
-            $this->kernel->getEnvironment(),
-            $this->kernel->isDebug(),
+            $this->environment,
+            $this->debug,
             $context
         );
 
