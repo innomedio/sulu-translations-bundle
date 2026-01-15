66c86d46388295d7896c15f265bc7731ab136d79
Ewald Vanderveken <ewald.vanderveken@innomedio.be>
2026-01-15 12:45:33 +0100

    run translation export via Symfony Application instead of Process

--- src/Infrastructure/Symfony/Translation/Export/CliExportAction.php
+++ src/Infrastructure/Symfony/Translation/Export/CliExportAction.php
@@ -4,49 +4,69 @@ declare(strict_types=1);
 
 namespace Phpro\SuluTranslationsBundle\Infrastructure\Symfony\Translation\Export;
 
+use App\Kernel;
 use Phpro\SuluTranslationsBundle\Domain\Action\ExportAction;
 use Phpro\SuluTranslationsBundle\Domain\Exception\ExportFailedException;
 use Phpro\SuluTranslationsBundle\Infrastructure\Symfony\Translation\Provider\DatabaseProviderFactory;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-final class CliExportAction implements ExportAction
+final readonly class CliExportAction implements ExportAction
 {
     public function __construct(
-        private readonly string $projectDir,
-        private readonly string $exportFormat,
+        private KernelInterface $kernel,
+        private string $exportFormat,
     ) {
     }
 
     public function __invoke(): string
     {
         try {
-            $pullTranslationsProcess = new Process([
-                'php',
-                'bin/console',
-                '--no-interaction',
-                'translation:pull',
-                DatabaseProviderFactory::PROVIDER_NAME,
-                '--format',
-                $this->exportFormat,
-                '--force',
-            ], timeout: 180);
-            $pullTranslationsProcess->setWorkingDirectory($this->projectDir);
-            $pullTranslationsProcess->mustRun();
+            $this->executeCommand(new ArrayInput([
+                'command' => 'translation:pull',
+                'provider' => DatabaseProviderFactory::PROVIDER_NAME,
+                '--format' => $this->exportFormat,
+                '--force' => true,
+                '--no-interaction' => true,
+            ]));
 
-            foreach (['adminconsole', 'websiteconsole'] as $console) {
-                $cacheCleanProcess = new Process([
-                    'php',
-                    'bin/'.$console,
-                    'cache:clear',
-                ], timeout: 180);
-                $cacheCleanProcess->setWorkingDirectory($this->projectDir);
-                $cacheCleanProcess->mustRun();
+            foreach (['admin', 'website'] as $context) {
+                $this->executeCommand(new ArrayInput([
+                    'command' => 'cache:clear',
+                ]), $context);
             }
 
-            return $pullTranslationsProcess->getOutput();
-        } catch (ProcessFailedException $exception) {
+            $this->executeCommand(new ArrayInput([
+                'command' => 'fos:httpcache:clear',
+            ]));
+
+            return 'Alle vertalingen zijn aangepast';
+        } catch (\Throwable $exception) {
             throw ExportFailedException::create($exception);
+        }
+    }
+
+    private function executeCommand(ArrayInput $arrayInput, string $context = 'website'): void
+    {
+        $kernel = new Kernel(
+            $this->kernel->getEnvironment(),
+            $this->kernel->isDebug(),
+            $context
+        );
+
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $pullOutput = new BufferedOutput();
+
+        $pullInput = $arrayInput;
+
+        $exitCode = $application->run($pullInput, $pullOutput);
+
+        if ($exitCode !== 0) {
+            throw new \RuntimeException($pullOutput->fetch());
         }
     }
 }
